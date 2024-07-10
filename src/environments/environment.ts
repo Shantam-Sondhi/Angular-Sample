b2afc9358ea454fc379d7814e05750d291a8debd
Shantam-Sondhi <73696709+Shantam-Sondhi@users.noreply.github.com>
2024-07-10 14:44:16 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
-API_URL="https://api.example.com"
-API_KEY="your-api-key-2"
-AUTH_DOMAIN="auth.example.com"
-PROJECT_ID="your-project-id"
-STORAGE_BUCKET="your-storage-bucket-2"
+export const environment = {
+  API_URL: "https://api.example.com",
+  API_KEY: "your-api-key-2",
+  AUTH_DOMAIN: "auth.example.com",
+  PROJECT_ID: "your-project-id",
+  STORAGE_BUCKET: "your-storage-bucket-2"
+};
